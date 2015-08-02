fe014ef94120bbb735a563e1cda1dcfd03d3e6c6
Vincent <vincent.huck.pro@gmail.com>
2015-08-02 21:10:32 +0200

    Add tourists list and radar plot

--- src/AppBundle/Controller/ResultController.php
+++ src/AppBundle/Controller/ResultController.php
@@ -10,6 +10,7 @@ namespace AppBundle\Controller;
 
 
 use AppBundle\Entity\Result;
+use AppBundle\Entity\Tourist;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,11 +20,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class ResultController extends Controller
 {
     /**
+     * @Route("/tourists", name="list_tourists")
+     */
+    public function listAction()
+    {
+        $tourists = $this->getDoctrine()->getRepository('AppBundle:Tourist')->findAll();
+
+        return $this->render('result/tourist_list.html.twig', array('tourists' => $tourists));
+    }
+
+    /**
      * @Route("/tourist/{id}", name="result_user")
      */
-    public function listAction($id)
+    public function listTouristAction(Tourist $tourist)
     {
-        $tourist = $this->getDoctrine()->getRepository('AppBundle:Tourist')->find($id);
         $results = $tourist->getResults();
 
         /** @var Result $result */

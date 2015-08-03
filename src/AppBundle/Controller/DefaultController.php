9d57a5f30c01efaad158fc92ed2e8b738151e447
Simeon Bobylev <simeon.bobylev@cortex-it.ch>
2015-08-03 15:55:28 +0200

    FOSuser integration to AdminLTE template and some modifs in main tpl

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="home")
      */
     public function indexAction()
     {

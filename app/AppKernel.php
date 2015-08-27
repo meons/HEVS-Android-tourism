d7156e239256fa6cd0644ba59b08abcb89e58825
Dany Maillard <danymaillard93b@gmail.com>
2015-08-27 23:14:25 +0200

    #21 Add FOSJsRoutingBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new AppBundle\AppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
